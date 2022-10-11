dcba29aedb016e5229624dc4c8db469afcaeb69c
fara0000 <fexri_imanzade@mail.ru>
2022-10-11 19:42:45 +0300

    feat: added Layout and main structure of Snow, changed some icons from svg to png

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,69 @@
+import clsx from 'clsx';
+import React from 'react';
+import styles from './PageLayout.module.css';
+
+type ColumnProps = {
+    columnClassName?: string;
+    htmlId?: string;
+    children: React.ReactNode;
+};
+
+export const Column: React.FC<ColumnProps> = ({
+    columnClassName,
+    htmlId,
+    children,
+}) => (
+    <div className={clsx(styles.column, columnClassName)} id={htmlId}>
+        {children}
+    </div>
+);
+
+type HeaderPageLayoutProps = {
+    mainWrapperClassName?: string;
+    contentWrapperClassName?: string;
+    children: React.ReactNode;
+};
+
+export const HeaderPageLayout: React.FC<HeaderPageLayoutProps> = ({
+    mainWrapperClassName,
+    contentWrapperClassName,
+    children,
+}) => (
+    <div className={clsx(styles.wrapper, mainWrapperClassName)}>
+        {/* тут будет HEADER */}
+
+        <div className={clsx(styles.contentWrapper, contentWrapperClassName)}>
+            {children}
+        </div>
+    </div>
+);
+
+export const ColumnHeaderPageLayout: React.FC<HeaderPageLayoutProps & ColumnProps> = ({
+    mainWrapperClassName,
+    contentWrapperClassName,
+    children,
+    ...columnProps
+}) => (
+    <HeaderPageLayout mainWrapperClassName={mainWrapperClassName} contentWrapperClassName={contentWrapperClassName}>
+        <Column {...columnProps}>{children}</Column>
+    </HeaderPageLayout>
+);
+
+type ColumnFullHeightLayoutProps = ColumnProps & {
+    wrapperMix?: string;
+};
+
+export const ColumnFullHeightLayout: React.FC<ColumnFullHeightLayoutProps> = ({
+    wrapperMix,
+    children,
+    ...columnProps
+}) => (
+    <div className={clsx(styles.wrapper, wrapperMix)}>
+        <Column
+            {...columnProps}
+            columnClassName={clsx(styles.contentWrapper, columnProps.columnClassName)}
+        >
+            {children}
+        </Column>
+    </div>
+);
