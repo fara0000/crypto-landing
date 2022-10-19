17e74d0a00257af3b5ee4863b9ea0cc4f6b240a4
fara0000 <fexri_imanzade@mail.ru>
2022-10-20 01:14:28 +0300

    feat: adaptivity of sservice cards, all data about cards, flexing and containering all service cards

---
+++ src/utils/serviceData.ts
@@ -0,0 +1,29 @@
+export enum ServiceNames {
+    '10K NFT collection',
+    Staking,
+    'No code Solutions ',
+    Vesting,
+    Safemoon,
+    'NFT Marketplace',
+    Metaverse,
+    Rust,
+    GameFi,
+    Farming,
+    Crowdsale,
+    DEX
+}
+
+export enum ServiceDescriptions {
+    '10K NFT collection',
+    Staking,
+    'No code Solutions ',
+    Vesting,
+    Safemoon,
+    'NFT Marketplace',
+    Metaverse,
+    Rust,
+    GameFi,
+    Farming,
+    Crowdsale,
+    DEX
+}
